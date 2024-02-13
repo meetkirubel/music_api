cce66ef5e9c981d0102314b988f3a9cf721d91d3
meetkirubel <122673954+meetkirubel@users.noreply.github.com>
2024-02-13 17:14:22 +0300

    Update songs.js

--- src/api/routes/songs.js
+++ src/api/routes/songs.js
@@ -1,10 +1,14 @@
 const {Router} = require('express')
+const cors = require('cors');
 const { allSongs, songDetails } = require('../controller/songs')
 
 const router = Router()
+const corsOptions = {
+origin: '*',  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+};
 
-router.get("/",allSongs)
-router.get("/:id",songDetails)
+router.get("/",cors(corsOptions),allSongs)
+router.get("/:id",cors(corsOptions),songDetails)
 
 
 module.exports = {songsRoute:router}
